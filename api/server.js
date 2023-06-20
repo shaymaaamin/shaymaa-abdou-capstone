f7334a817a23a8891afc441882e49b662df0fb0e
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-06-20 05:03:21 -0400

    add database migration, seeds & routing endpoints

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const crud = require('./crud');
 const port = process.env.REACT_APP_BACKEND_PORT || 3001;
 
 app.use(cors());
@@ -10,13 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/assets', require('./assets'));
-app.use('/employees', require('./employees'));
-app.use('/faults', require('./faults'));
-app.use('/jobs', require('./jobs'));
-app.use('/priorities', require('./priorities'));
-app.use('/statuses', require('./statuses'));
-app.use('/types', require('./types'));
+app.use('/assets', crud('assets'));
+app.use('/employees', crud('employees'));
+app.use('/jobs', crud('jobs'));
+
+app.use('/faults', crud('faults'));
+app.use('/priorities', crud('priorities'));
+app.use('/skills', crud('skills'));
+app.use('/statuses', crud('statuses'));
+app.use('/types', crud('types'));
 
 
 app.listen(port, () => {
